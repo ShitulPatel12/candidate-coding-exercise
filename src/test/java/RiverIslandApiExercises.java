465e0137497abe82e42d301c3f61070f7c156d90
ShitulPatel12 <Shitul.Patel@river-island.com>
2023-02-24 11:59:08 +0000

    Update RiverIslandApiExercises.java

--- src/test/java/RiverIslandApiExercises.java
+++ src/test/java/RiverIslandApiExercises.java
@@ -1,8 +1,6 @@
-import org.junit.Test;
+**API TEST**
 
-/**
 [thecatapi](https://docs.thecatapi.com/api-reference) is a free restful service that helps you to store and retrieve cat images.
-
 * PROTOCOL: https://
 * HOST: api.thecatapi.com
 * VERSION: v1
@@ -17,63 +15,60 @@ import org.junit.Test;
 | /votes/{vote_id} | GET    | Get one specific Vote belonging to your Account  |
 | /votes/{vote_id} | DELETE | Delete a Vote from your Account                  |
 
-*/
+**Exercise 1:**
 
-public class RiverIslandApiExercises {
+Configure a basic HTTP client to be able to make REST call (You can setup python based framework/library)
 
- @Test public void test1() {
-     /**
-     * Exercise 1: Configure a basic HTTP client to be able to make REST call (you can setup restassured ar any other desired library
-     *
-     * Requirement
-     *      * Update gradle file with appropriate dependency
-            * Import necessary dependency library
-            * `GET /votes` and store the response.
-              * Create an object to unmarshal the result in it.
-              * Verify `200` response status code
-              * Verify response length more than 0.
-       Objectives 
-	    * Be able to configure and make an http request
-     */
- }
+**Requirement**
 
- @Test public void test2() {
-      /**
-      * Exercise 2: Create a new vote `POST /votes`.
-      *
-      * Requirement
-      *      * Verify `200` response status code,
-             * Verify response body match expected value `{"message": "SUCCESS"}`,
-             * Verify {id} in response is not empty and store the value.
-        Objectives
-         * POST
-      */
- }
+- Import necessary dependency library
+- GET /votes` and store the response.
+- Verify `200` response status code
+- Verify response length more than 0.
 
-  @Test public void test3() {
-    /**
-    * Exercise 3: Use the *{id}* created at previous point to `GET /votes/{id}`.
-    *
-    * Requirement
-    *      * Verify `200` response status code,
-           * *{id}* in response match *{id}* in request.
-      Objectives
-       * GET
-    */
-  }
+**Objectives**
 
-  @Test public void test4() {
-      /**
-      * Exercise 4: Delete the vote at previous point `DELETE /votes/{vote_id}`
+- Be able to configure and make an http request
 
-      *
-      * Requirement
-      *      * Verify `200` response status code,
-             * Verify response body match expected value `{"message": "SUCCESS"}`,
-        Objectives
-         * DELETE
-      */
-  }
-}
 
+**Exercise 2:**
+
+Create a new vote `POST /votes`.
+
+**Requirement**
+
+- Verify `201` response status code,
+- Verify response body match expected value `{"message": "SUCCESS"}`,
+- Verify {id} in response is not empty and store the value.
+
+**Objectives**
+- POST
+
+
+**Exercise 3:**
+
+Use the *{id}* created at previous point to `GET /votes/{id}`.
+
+**Requirement**
+
+- Verify `200` response status code,
+- *{id}* in response match *{id}* in request.
+
+**Objectives**
+- GET
+
+***Bonus:***
+
+**Exercise 4:**
+
+Delete the vote at previous point `DELETE /votes/{vote_id}`
+
+**Requirement**
+
+- Verify `200` response status code,
+- Verify response body match expected value `{"message": "SUCCESS"}`
+
+**Objectives**
+
+- DELETE
 
